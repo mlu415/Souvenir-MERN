e0195fb6aa8da040b090ca0853aaa0edf6b7a701
michelle <mlu415@aucklanduni.ac.nz>
2022-08-23 14:24:24 +1200

    controller update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,2 +1,9 @@
+import PostMessage from "../models/postMessage.js"
+
 export const getPosts = (req, res)=>{
-    res.send("Why would you do webdev")}
+    res.send("Why would you do webdev");
+}
+
+export const createPost = (req, res)=>{
+    res.send("Post Creation");
+}
