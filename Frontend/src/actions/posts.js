86d0bec0c31c64ae278166d2dd20fa5adb76e553
michelle <mlu415@aucklanduni.ac.nz>
2022-08-30 14:26:51 +1200

    createpost not working

--- Frontend/src/actions/posts.js
+++ Frontend/src/actions/posts.js
@@ -5,16 +5,23 @@ import * as api from "../api";
 //uses redux thunk as dealing with asynch logic
 export const getPosts = () => async(dispatch)=>{
     try {
-        const{data } = await api.fetchPosts();
-        dispatch({type: "FETCH_ALL", payload:data}); 
+        const{data} = await api.fetchPosts();
+        dispatch({type: `FETCH_ALL`, payload:data}); 
         
     } catch (error) {
-        console.log(error.message)
+        console.log(error)
         
     }
+}
 
 
+export const createPost = (post) => async (dispatch) =>{
+    try {
+        const {data } = await api.createPost(post);
+        dispatch({type: "CREATE", payload:data});
+    } catch (error) {
+        console.log(error)
+    }
 
-    const action = {type: "FETCH_ALL", payload:[]}
 
 }
--- Frontend/src/components/Form/Form.js
+++ Frontend/src/components/Form/Form.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import FileBase from "react-file-base64";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import {useDispatch} from "react-redux"
 
 import useStyles from "./styles";
+import e from "cors";
+import {createPost} from "../../actions/posts";
 
-const Form = () => { const [postData, setPostData] = useState({ creator: "", title: "", message: "", tags: "", selectedFile: "", });
+const Form = (e) => { 
+  const [postData, setPostData] = useState({ creator: "", title: "", message: "", tags: "", selectedFile: "", });
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   const handleSubmit = () => {
-    
+    e.preventDefault();
+
+    dispatch(createPost(postData));
   };
 
   const clear = () => {
