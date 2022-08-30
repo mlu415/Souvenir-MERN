40cb09aa3709679cb353f658218b722e4a706590
michelle <mlu415@aucklanduni.ac.nz>
2022-08-30 15:58:38 +1200

    update posts.js

--- Frontend/src/actions/posts.js
+++ Frontend/src/actions/posts.js
@@ -3,25 +3,23 @@ import * as api from "../api";
 // Action creators
 
 //uses redux thunk as dealing with asynch logic
-export const getPosts = () => async(dispatch)=>{
+export const getPosts = () => async (dispatch) => {
     try {
-        const{data} = await api.fetchPosts();
-        dispatch({type: `FETCH_ALL`, payload:data}); 
-        
+      const { data } = await api.fetchPosts();
+  
+      dispatch({ type: 'FETCH_ALL', payload: data });
     } catch (error) {
-        console.log(error)
-        
+      console.log(error.message);
     }
-}
+  };
 
 
-export const createPost = (post) => async (dispatch) =>{
+  export const createPost = (post) => async (dispatch) => {
     try {
-        const {data } = await api.createPost(post);
-        dispatch({type: "CREATE", payload:data});
+      const { data } = await api.createPost(post);
+  
+      dispatch({ type: "CREATE", payload: data });
     } catch (error) {
-        console.log(error)
+      console.log(error.message);
     }
-
-
-}
+  };
