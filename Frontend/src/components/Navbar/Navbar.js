81fd74911017e415e26ce7794c504839b8b42f7b
michelle <mlu415@aucklanduni.ac.nz>
2022-08-31 22:07:23 +1200

    broken appbar

--- Frontend/src/components/Navbar/Navbar.js
+++ Frontend/src/components/Navbar/Navbar.js
@@ -1,4 +1,7 @@
+//This file adds in the navigation bar as a seperate component for the website
+//contains the login, title and logo
 import React from "react";
+import {Link} from 'react-router-dom'
 import { AppBar, Typography } from "@material-ui/core";
 import useStyles from './styles'
 import memories from "../../images/memories.png"
@@ -8,7 +11,9 @@ const Navbar = () => {
     const classes = useStyles();
     return (
         <AppBar className = {classes.appBar} position = "static" color="inherit">
-        <Typography className = {classes.heading} variant = "h2" align = "center">Memory Store</Typography>
+        <div>
+        <Typography component = {Link} to = '/' className = {classes.heading} variant = "h2" align = "center">Memory Store</Typography>
+        </div>
         <img className = {classes.image} src = {memories} alt = "memories" height="60"/>
     </AppBar>
     )
