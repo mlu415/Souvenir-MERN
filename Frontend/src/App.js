5f319d869062f0dcddd0b3c7dc119708db160e6e
michelle <mlu415@aucklanduni.ac.nz>
2022-09-01 00:23:01 +1200

    fixed login and multipage route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,50 +1,24 @@
-import React, {useState, useEffect} from "react";
-import {Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import React  from "react";
+import {Container } from "@material-ui/core";
 
-//getting an action
-import {getPosts} from "./actions/posts"
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyles from "./styles"
-
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from "./components/Auth/Auth";
 
 
-const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    //dispatches changes as soon as currentId is changed
-    useEffect(()=> {
-        dispatch(getPosts());
-    },[currentId,dispatch]);
-
-    return(
-
+const App = () => (
+    <BrowserRouter>
         <Container maxwidth = "lg">
             <Navbar />
-            <Grow in>
-                <Container>
-                    <Grid className = {classes.mainContainer} container  justifyContent = "space-between" alignItems = "stretch" spacing = {3}>
-                        <Grid item xs={12} sm = {7}>
-                            <Posts setCurrentId = {setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm = {4}>
-                            <Form currentId = {currentId}  setCurrentId = {setCurrentId} />
-                        </Grid>
-                    </Grid>
+            <Switch>
+                <Route path = '/' exact component = {Home} />
+                <Route path = '/auth' exact component = {Auth} />
+            </Switch>
+        </Container>    
+    </BrowserRouter>
 
-                </Container>
-
-            </Grow>
-
-        </Container>
-        
-    );
-}
+)
 
 export default App;
---
+++ Frontend/src/components/Home/Home.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from "react";
+import {Container,  Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+//getting an action
+import {getPosts} from "../../actions/posts"
+
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+//import useStyles from "./styles"
+
+
+const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    //const classes = useStyles();
+    const dispatch = useDispatch();
+
+    //dispatches changes as soon as currentId is changed
+    useEffect(()=> {
+        dispatch(getPosts());
+    },[currentId,dispatch]);
+
+    return(
+
+    <Grow in>
+    <Container>
+        <Grid  container  justifyContent = "space-between" alignItems = "stretch" spacing = {3}>
+            <Grid item xs={12} sm = {7}>
+                <Posts setCurrentId = {setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm = {4}>
+                <Form currentId = {currentId}  setCurrentId = {setCurrentId} />
+            </Grid>
+        </Grid>
+
+    </Container>
+
+</Grow>
+    )
+}
+
+
+export default Home;
